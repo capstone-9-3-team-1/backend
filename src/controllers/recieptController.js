f9faf46928a8df9655d90c254682e7d42472ab6d
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-09-07 10:11:06 -0400

    Updates reciept controller

--- src/controllers/recieptController.js
+++ src/controllers/recieptController.js
@@ -7,18 +7,23 @@ const receiptRouter = express.Router();
 receiptRouter
   .route("/")
   .get(async (req, res) => {
-    const allReceipts = await prisma.receipt.findMany();
+    const allReceipts = await prisma.receipt.findMany({
+      include: {
+        products: true,
+      },
+    });
     res.json(allReceipts);
   })
   .post(async (req, res) => {
-    const newReceipt = await prisma.receipt.create({ data: req.body });
+    const { userId } = req.body;
+    const newReceipt = await prisma.receipt.create({ data: { userId } });
     res.json(newReceipt);
   });
 
 receiptRouter.delete("/:id", async (req, res) => {
   const id = req.params.id;
   const deletedReceipt = await prisma.receipt.delete({
-    where: { id: parseInt(id) },
+    where: { id: id },
   });
   res.json(deletedReceipt);
 });
